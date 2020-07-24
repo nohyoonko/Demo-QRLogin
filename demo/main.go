e572cfb58a0cfbd94501a5e7dfd629239b4cb8ec
nohyoon <kny_1011@naver.com>
2020-07-24 14:10:14 +0900

    using template to handle form value

--- demo/main.go
+++ demo/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/pat"
 	"github.com/urfave/negroni"
 )
 
+type QR struct {
+	Num string
+}
+
 func postQRHandler(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("public/mobile.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	qrNum := r.FormValue("qr_num")
 	fmt.Println("POST " + qrNum) //qr_num
 
-	http.ServeFile(w, r, "public/mobile.html")
+	tmp.Execute(w, QR{qrNum})
+
+	//http.ServeFile(w, r, "public/mobile.html")
 	//http.Redirect(w, r, "/mobile.html", http.StatusMovedPermanently) //Get, 301
 	//http.Redirect(w, r, "/mobile.html", http.StatusTemporaryRedirect) //Post, 307
 }
